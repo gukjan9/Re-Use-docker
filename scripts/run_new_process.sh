545dee8caeda8fd382b969210a6e4a9063bf3d11
GukJang <9ukjang@gmail.com>
2024-01-11 21:33:35 +0900

    echo 문구 수정

--- scripts/run_new_process.sh
+++ scripts/run_new_process.sh
@@ -1,7 +1,7 @@
-echo "Starting run_new_process.sh"
+echo "***** Executing run_new_process.sh *****"
 
 docker-compose down
 docker-compose pull
 docker-compose up -d
 
-echo "run_new_process.sh Ended"
+echo "***** run_new_process.sh Ended *****"
