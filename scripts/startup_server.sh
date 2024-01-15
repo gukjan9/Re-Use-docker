ca5d086610c441678e3810c69e66dc4064c7d689
GukJang <gukjang1997@gmail.com>
2024-01-15 17:33:40 +0900

    startup_server.sh 구성

--- scripts/startup_server.sh
+++ scripts/startup_server.sh
@@ -1,7 +1,12 @@
 echo "***** Executing startup_server.sh *****"
 
-# 재부팅 될 때마다 실행되게끔
 # 포트포워딩
 sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
+
+# Docker 재시작
+source restart_docker.sh
+
+# Process 실행
+source run_new_process.sh
 
 echo "***** startup_server.sh Ended *****"
