da2ece734b1a6d7467260d19d01c44f12b5fe8aa
GukJang <gukjang1997@gmail.com>
2024-01-22 13:39:45 +0900

    스크립트 확인 로직 변경

--- scripts/set_ssh_port.sh
+++ scripts/set_ssh_port.sh
@@ -11,12 +11,17 @@ else
     exit 1
 fi
 
+TARGET_FILE = /etc/ssh/sshd_config
+
+# 원본 파일의 복사본 생성
+cp "$TARGET_FILE" "${TARGET_FILE}.bak"
+
 # /etc/ssh/sshd_config 파일 수정
 # '#'으로 시작하지 않으면서 'PORT $SSH_PORT'가 아닌 줄을 찾아 제거
-sudo sed -i "/^#/!{/PORT[[:space:]]$SSH_PORT/!d}" /etc/ssh/sshd_config
+sudo sed -i "/^#/!{/PORT[[:space:]]$SSH_PORT/!d}" "$TARGET_FILE"
 
 # 원본 파일과 수정된 파일 비교
-if ! cmp -s /etc/ssh/sshd_config.bak /etc/ssh/sshd_config; then
+if cmp -s "${FILE}.bak" "$FILE"; then
     echo "Previous port configuration has been deleted"
     # 해당 포트를 sshd_config에 추가
     echo "PORT $SSH_PORT" | sudo tee -a /etc/ssh/sshd_config > /dev/null
