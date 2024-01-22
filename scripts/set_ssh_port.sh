7b7f5115e1fcc7f2ee91cca45840197f15d0d725
GukJang <gukjang1997@gmail.com>
2024-01-22 13:28:09 +0900

    port 정규식 변경

--- scripts/set_ssh_port.sh
+++ scripts/set_ssh_port.sh
@@ -13,7 +13,7 @@ fi
 
 # /etc/ssh/sshd_config 파일 수정
 # '#'으로 시작하지 않으면서 'PORT $SSH_PORT'가 아닌 줄을 찾아 제거
-sudo sed -i "/^[^#]*PORT $SSH_PORT\>/d" /etc/ssh/sshd_config
+sudo sed "/^#/!{/PORT/ {/\s$SSH_PORT\s/!d;}}" /etc/ssh/sshd_config
 
 # 원본 파일과 수정된 파일 비교
 if ! cmp -s /etc/ssh/sshd_config.bak /etc/ssh/sshd_config; then
