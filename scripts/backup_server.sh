f9f1d350cb71f51c74d6b07bba8b999fe48a2cf7
GukJang <9ukjang@gmail.com>
2024-01-23 23:59:27 +0900

    exec 명렁어 수정

--- scripts/backup_server.sh
+++ scripts/backup_server.sh
@@ -21,7 +21,7 @@ mkdir -p "$folder_path" && echo "Directory created: $folder_path"
 
 # MySQL 백업
 echo "Backing up MySQL data... [3/6]"
-docker exec $MYSQL_CONTAINER_NAME mysqldump -u root -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE > /home/backup.sql
+docker exec -i $MYSQL_CONTAINER_NAME bash -c "mysqldump -u root -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE > /home/backup.sql"
 sudo docker cp $MYSQL_CONTAINER_NAME:/home/backup.sql  /home/ubuntu/backup/backup.sql
 
 # sshpass 설치
