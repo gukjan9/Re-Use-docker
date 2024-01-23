bd8ff91f84ffe56625b6438f5be38bb236310128
GukJang <9ukjang@gmail.com>
2024-01-23 17:55:24 +0900

    mysql docker exec 명령어 수정

--- scripts/restore_data.sh
+++ scripts/restore_data.sh
@@ -12,11 +12,11 @@ else
 fi
 
 # Docker 볼륨 생성
-echo "Creating Docker volume..."
+echo "Creating Docker volume... [2/8]"
 docker volume create ${MYSQL_DATA}
 
 # MySQL pull
-echo "Pulling MySQL image..."
+echo "Pulling MySQL image... [3/8]"
 docker pull mysql
 
 # restore 용 MySQL 환경변수 재설정
@@ -25,27 +25,23 @@ MYSQL_ROOT_PASSWORD="1234"
 MYSQL_CONTAINER_NAME="restore_container"
 
 # MySQL 실행
-echo "Running MySQL container..."
+echo "Running MySQL container... [4/8]"
 docker run --name $MYSQL_CONTAINER_NAME -v $MYSQL_VOLUME:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD -d $MYSQL_IMAGE_NAME
 
 # 백업 파일 복사
-echo "Copying backup file to the container..."
+echo "Copying backup file to the container... [5/8]"
 docker cp /home/$TARGET_SERVER_USERNAME/backup/backup.sql $MYSQL_CONTAINER_NAME:/var/lib/mysql/backup.sql
 
 # MySQL에 접속하여 백업 데이터 복원
-echo "Restoring data from backup..."
-docker exec -i $MYSQL_CONTAINER_NAME /bin/bash << EOF
-mysql -u root -p $MYSQL_ROOT_PASSWORD << EOF
-source /var/lib/mysql/backup.sql
-EOF
-EOF
+echo "Restoring data from backup... [6/8]"
+docker exec -i $MYSQL_CONTAINER_NAME /bin/bash -c "mysql -u root -p$MYSQL_ROOT_PASSWORD < /var/lib/mysql/backup.sql"
 
 # MySQL 컨테이너 중지
-echo "Stopping MySQL container..."
+echo "Stopping MySQL container... [7/8]"
 docker stop $MYSQL_CONTAINER_NAME
 
 # MySQL 이미지 삭제
-echo "Deleting MySQL image..."
+echo "Deleting MySQL image... [8/8]"
 docker rmi $MYSQL_IMAGE_NAME
 
 echo "***** restore_data.sh Ended *****"
