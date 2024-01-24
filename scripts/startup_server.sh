200cd646c096df9ced887d7d4bc243f65adb8540
GukJang <9ukjang@gmail.com>
2024-01-24 17:14:31 +0900

    #!/bin/sh 추가

--- scripts/startup_server.sh
+++ scripts/startup_server.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 echo "***** Executing startup_server.sh *****"
 
 # 포트포워딩
@@ -6,10 +8,10 @@ sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
 
 # Docker 재시작
 echo "Restarting docker... [2/3]"
-source ~/scripts/restart_docker.sh
+. ~/scripts/restart_docker.sh
 
 # Process 실행
 echo "Running server... [3/3]"
-source ~/scripts/run_new_process.sh
+. ~/scripts/run_new_process.sh
 
 echo "***** startup_server.sh Ended *****"
