0af1d608a2f7a70fc97e87e89ce4d0779bfdc244
GukJang <9ukjang@gmail.com>
2024-01-24 23:29:39 +0900

    os 버전에 따른 docker-compose 실행

--- scripts/run_new_process.sh
+++ scripts/run_new_process.sh
@@ -2,8 +2,19 @@
 
 echo "***** Executing run_new_process.sh *****"
 
-docker-compose down
-docker-compose pull
-docker-compose up -d
+# OS 버전을 확인
+os_version=$(uname -a)
+
+if [[ $os_version == *"armv7l"* ]] || [[ $os_version == *"raspi"* ]]; then
+  echo "$os_version"
+  docker-compose -f docker-compose.pi.yml down
+  docker-compose -f docker-compose.pi.yml pull
+  docker-compose -f docker-compose.pi.yml up -d
+else
+  echo "$os_version"
+  docker-compose -f docker-compose.yml down
+  docker-compose -f docker-compose.yml pull
+  docker-compose -f docker-compose.yml up -d
+fi
 
 echo "***** run_new_process.sh Ended *****"
