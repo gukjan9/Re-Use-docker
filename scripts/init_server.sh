fdaa95e5a183e1434d51514c51fd83530d63ebb9
GukJang <9ukjang@gmail.com>
2024-01-24 17:54:32 +0900

    apt -> apt-get

--- scripts/init_server.sh
+++ scripts/init_server.sh
@@ -67,7 +67,7 @@ echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-
 echo "Installing Docker-Compose... [4/7]"
 # sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 # (pi)
-sudo apt install -y python3-pip
+sudo apt-get install -y python3-pip
 sudo pip3 install docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
