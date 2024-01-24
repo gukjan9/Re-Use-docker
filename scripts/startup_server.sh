162f1499c3816df1843d5ea71f00001cf99bd48e
GukJang <9ukjang@gmail.com>
2024-01-24 22:21:51 +0900

    /etc/resolv.cnf 에 nameserver 추가

--- scripts/startup_server.sh
+++ scripts/startup_server.sh
@@ -6,6 +6,16 @@ echo "***** Executing startup_server.sh *****"
 echo "Setting port forwarding... [1/3]"
 sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
 
+# (pi)
+# /etc/resolv.conf 파일을 확인하고, nameserver가 없으면 추가
+if ! grep -q "nameserver 8.8.8.8" /etc/resolv.conf; then
+    echo "nameserver 8.8.8.8" | sudo tee -a /etc/resolv.conf
+fi
+
+if ! grep -q "nameserver 8.8.4.4" /etc/resolv.conf; then
+    echo "nameserver 8.8.4.4" | sudo tee -a /etc/resolv.conf
+fi
+
 # Docker 재시작
 echo "Restarting docker... [2/3]"
 source ~/scripts/restart_docker.sh
