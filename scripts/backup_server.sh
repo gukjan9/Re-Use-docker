fed795fa5aa3978047aa689c72ccaf197bcf9240
GukJang <gukjang1997@gmail.com>
2024-02-14 05:37:57 +0900

    SSH_USERNAME 으로 환경변수화

--- scripts/backup_server.sh
+++ scripts/backup_server.sh
@@ -18,13 +18,13 @@ echo "Uploading latest images... [2/6]"
 #source ./scripts/upload_latest_images.sh
 
 # backup 폴더 생성
-folder_path="/home/ubuntu/backup"
-mkdir -p "$folder_path" && echo "Directory created: $folder_path"
+folder_path="/home/$SSH_USERNAME/backup"
+mkdir -p "$folder_path" && echo "Backup directory created"
 
 # MySQL 백업
 echo "Backing up MySQL data... [3/6]"
 docker exec -i $MYSQL_CONTAINER_NAME bash -c "mysqldump -u $MYSQL_DATABASE_USERNAME -p$MYSQL_DATABASE_PASSWORD $MYSQL_DATABASE > /home/backup.sql"
-docker cp $MYSQL_CONTAINER_NAME:/home/backup.sql /home/ubuntu/backup/backup.sql
+docker cp $MYSQL_CONTAINER_NAME:/home/backup.sql /home/$SSH_USERNAME/backup/backup.sql
 
 # sshpass 설치
 sudo apt-get install sshpass
@@ -34,10 +34,10 @@ echo "Creating backup directory on the target server for migration... [4/6]"
 sshpass -p $TARGET_SERVER_PASSWORD ssh $TARGET_SERVER_USERNAME@$TARGET_SERVER_IP -p $TARGET_SERVER_PORT -o StrictHostKeyChecking=no "mkdir -p /home/$TARGET_SERVER_USERNAME/backup" && echo "Backup directory created successfully on the target server"
 
 echo "Transferring MySQL backup data... [5/6]"
-sshpass -p $TARGET_SERVER_PASSWORD scp -P $TARGET_SERVER_PORT /home/ubuntu/backup/backup.sql $TARGET_SERVER_USERNAME@$TARGET_SERVER_IP:/home/$TARGET_SERVER_USERNAME/backup && echo "Transfer successful!" || echo "Transfer failed"
+sshpass -p $TARGET_SERVER_PASSWORD scp -P $TARGET_SERVER_PORT /home/$SSH_USERNAME/backup/backup.sql $TARGET_SERVER_USERNAME@$TARGET_SERVER_IP:/home/$TARGET_SERVER_USERNAME/backup && echo "Transfer successful!" || echo "Transfer failed"
 
 echo "Transferring env files... [6/6]"
-sshpass -p $TARGET_SERVER_PASSWORD scp -P $TARGET_SERVER_PORT /home/ubuntu/.env $TARGET_SERVER_USERNAME@$TARGET_SERVER_IP:/home/$TARGET_SERVER_USERNAME/ && echo "Transfer successful!" || echo "Transfer failed"
-sshpass -p $TARGET_SERVER_PASSWORD scp -P $TARGET_SERVER_PORT /home/ubuntu/scripts/set_env.sh $TARGET_SERVER_USERNAME@$TARGET_SERVER_IP:/home/$TARGET_SERVER_USERNAME/ && echo "Transfer successful!" || echo "Transfer failed"
+sshpass -p $TARGET_SERVER_PASSWORD scp -P $TARGET_SERVER_PORT /home/$SSH_USERNAME/.env $TARGET_SERVER_USERNAME@$TARGET_SERVER_IP:/home/$TARGET_SERVER_USERNAME/ && echo "Transfer successful!" || echo "Transfer failed"
+sshpass -p $TARGET_SERVER_PASSWORD scp -P $TARGET_SERVER_PORT /home/$SSH_USERNAME/scripts/set_env.sh $TARGET_SERVER_USERNAME@$TARGET_SERVER_IP:/home/$TARGET_SERVER_USERNAME/ && echo "Transfer successful!" || echo "Transfer failed"
 
 echo "***** backup_server.sh Ended *****"
