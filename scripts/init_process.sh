0de15b96fd1387967b5013c81761a0578a16fa2a
GukJang <9ukjang@gmail.com>
2024-02-22 21:17:50 +0900

    init_process.h blue green 업로드 시간으로 구분해서 실행

--- scripts/init_process.sh
+++ scripts/init_process.sh
@@ -2,6 +2,17 @@
 
 echo "***** Executing init_process.sh *****"
 
+# .env 파일 로드
+echo "Loading .env... [1/6]"
+ENV_FILE="$HOME/.env"
+if [ -f "$ENV_FILE" ]; then
+    export $(cat "$ENV_FILE" | xargs)
+    echo "$ENV_FILE exported"
+else
+    echo "Cannot find $ENV_FILE"
+    exit 1
+fi
+
 # OS 버전을 확인
 os_version=$(uname -a)
 
@@ -9,12 +20,33 @@ if [[ $os_version == *"armv7l"* ]] || [[ $os_version == *"raspi"* ]]; then
   echo "$os_version"
   docker compose -f docker-compose.pi.yml down
   docker compose -f docker-compose.pi.yml pull
-  docker compose -f docker-compose.pi.yml up -d
+
+  BLUE_CREATED=$(docker inspect --format '{{ .Created }}' "${DOCKER_USERNAME}/${SPRING_CONTAINER_NAME}:blue")
+  GREEN_CREATED=$(docker inspect --format '{{ .Created }}' "${DOCKER_USERNAME}/${SPRING_CONTAINER_NAME}:green")
+
+  if [[ "$BLUE_CREATED" > "$GREEN_CREATED" ]]; then
+    echo "Running SPRING-BLUE"
+    docker compose -f docker-compose.pi.yml up -d nginx redis mysql spring-blue
+  else
+    echo "Running SPRING_GREEN"
+    docker compose -f docker-compose.pi.yml up -d nginx redis mysql spring-green
+  fi
+
 else
   echo "$os_version"
   docker-compose -f docker-compose.yml down
   docker-compose -f docker-compose.yml pull
-  docker-compose -f docker-compose.yml up -d
+
+  BLUE_CREATED=$(docker inspect --format '{{ .Created }}' "${DOCKER_USERNAME}/${SPRING_CONTAINER_NAME}:blue")
+  GREEN_CREATED=$(docker inspect --format '{{ .Created }}' "${DOCKER_USERNAME}/${SPRING_CONTAINER_NAME}:green")
+
+  if [[ "$BLUE_CREATED" > "$GREEN_CREATED" ]]; then
+    echo "Running SPRING-BLUE"
+    docker-compose -f docker-compose.yml up -d nginx redis mysql spring-blue
+  else
+    echo "Running SPRING_GREEN"
+    docker-compose -f docker-compose.yml up -d nginx redis mysql spring-green
+  fi
 fi
 
 echo "***** init_process.sh Ended *****"
