4e7559e9797a44f063f722ea59ab851e943fb632
GukJang <9ukjang@gmail.com>
2024-03-17 01:46:03 +0900

    pi / ec2 분리

--- scripts/backup_server.sh
+++ scripts/backup_server.sh
@@ -26,21 +26,45 @@ echo "Backing up MySQL data... [3/7]"
 docker exec -i $MYSQL_CONTAINER_NAME bash -c "mysqldump -u $MYSQL_DATABASE_USERNAME -p$MYSQL_DATABASE_PASSWORD $MYSQL_DATABASE > /home/backup.sql"
 docker cp $MYSQL_CONTAINER_NAME:/home/backup.sql /home/$SSH_USERNAME/backup/backup.sql
 
-# sshpass 설치
-sudo apt-get install sshpass
+# OS 버전을 확인
+os_version=$(uname -a)
 
-# 이전하려는 서버에 연결 및 파일 전송
-echo "Creating backup directory on the target server for migration... [4/7]"
-sshpass -p $TARGET_SERVER_PASSWORD ssh $TARGET_SERVER_USERNAME@$TARGET_SERVER_IP -p $TARGET_SERVER_PORT -o StrictHostKeyChecking=no "mkdir -p /home/$TARGET_SERVER_USERNAME/backup" && echo "Backup directory created successfully on the target server"
+if [[ $os_version == *"armv7l"* ]] || [[ $os_version == *"raspi"* ]]; then
+  echo "$os_version"
 
-echo "Transferring MySQL backup data... [5/7]"
-sshpass -p $TARGET_SERVER_PASSWORD scp -P $TARGET_SERVER_PORT /home/$SSH_USERNAME/backup/backup.sql $TARGET_SERVER_USERNAME@$TARGET_SERVER_IP:/home/$TARGET_SERVER_USERNAME/backup && echo "Transfer successful!" || echo "Transfer failed"
+  # sshpass 설치
+  sudo apt-get install sshpass
 
-echo "Transferring Nginx data... [6/7]"
-sshpass -p $TARGET_SERVER_PASSWORD scp -P $TARGET_SERVER_PORT /home/$SSH_USERNAME/nginx/ $TARGET_SERVER_USERNAME@$TARGET_SERVER_IP:/home/$TARGET_SERVER_USERNAME/ && echo "Transfer successful!" || echo "Transfer failed"
+  # 이전하려는 서버에 연결 및 파일 전송
+  echo "Creating backup directory on the target server for migration... [4/7]"
+  sshpass -p $TARGET_SERVER_PASSWORD ssh $TARGET_SERVER_USERNAME@$TARGET_SERVER_IP -p $TARGET_SERVER_PORT -o StrictHostKeyChecking=no "mkdir -p /home/$TARGET_SERVER_USERNAME/backup" && echo "Backup directory created successfully on the target server"
 
-echo "Transferring env files... [7/7]"
-sshpass -p $TARGET_SERVER_PASSWORD scp -P $TARGET_SERVER_PORT /home/$SSH_USERNAME/.env $TARGET_SERVER_USERNAME@$TARGET_SERVER_IP:/home/$TARGET_SERVER_USERNAME/ && echo "Transfer successful!" || echo "Transfer failed"
-sshpass -p $TARGET_SERVER_PASSWORD scp -P $TARGET_SERVER_PORT /home/$SSH_USERNAME/scripts/set_env.sh $TARGET_SERVER_USERNAME@$TARGET_SERVER_IP:/home/$TARGET_SERVER_USERNAME/ && echo "Transfer successful!" || echo "Transfer failed"
+  echo "Transferring MySQL backup data... [5/7]"
+  sshpass -p $TARGET_SERVER_PASSWORD scp -P $TARGET_SERVER_PORT /home/$SSH_USERNAME/backup/backup.sql $TARGET_SERVER_USERNAME@$TARGET_SERVER_IP:/home/$TARGET_SERVER_USERNAME/backup && echo "Transfer successful!" || echo "Transfer failed"
+
+  echo "Transferring Nginx data... [6/7]"
+  sshpass -p $TARGET_SERVER_PASSWORD scp -P $TARGET_SERVER_PORT /home/$SSH_USERNAME/nginx/ $TARGET_SERVER_USERNAME@$TARGET_SERVER_IP:/home/$TARGET_SERVER_USERNAME/ && echo "Transfer successful!" || echo "Transfer failed"
+
+  echo "Transferring env files... [7/7]"
+  sshpass -p $TARGET_SERVER_PASSWORD scp -P $TARGET_SERVER_PORT /home/$SSH_USERNAME/.env $TARGET_SERVER_USERNAME@$TARGET_SERVER_IP:/home/$TARGET_SERVER_USERNAME/ && echo "Transfer successful!" || echo "Transfer failed"
+  sshpass -p $TARGET_SERVER_PASSWORD scp -P $TARGET_SERVER_PORT /home/$SSH_USERNAME/scripts/set_env.sh $TARGET_SERVER_USERNAME@$TARGET_SERVER_IP:/home/$TARGET_SERVER_USERNAME/ && echo "Transfer successful!" || echo "Transfer failed"
+
+else
+  echo "$os_version"
+
+  # 이전하려는 서버에 연결 및 파일 전송
+  echo "Creating backup directory on the target server for migration... [4/7]"
+  ssh -i $TARGET_SERVER_KEYFILE_DIR $TARGET_SERVER_USERNAME@$TARGET_SERVER_IP -p $TARGET_SERVER_PORT -o StrictHostKeyChecking=no "mkdir -p /home/$TARGET_SERVER_USERNAME/backup" && echo "Backup directory created successfully on the target server"
+
+  echo "Transferring MySQL backup data... [5/7]"
+  scp -i $TARGET_SERVER_KEYFILE_DIR -P $TARGET_SERVER_PORT /home/$SSH_USERNAME/backup/backup.sql $TARGET_SERVER_USERNAME@$TARGET_SERVER_IP:/home/$TARGET_SERVER_USERNAME/backup && echo "Transfer successful!" || echo "Transfer failed"
+
+  echo "Transferring Nginx data... [6/7]"
+  scp -i $TARGET_SERVER_KEYFILE_DIR -P $TARGET_SERVER_PORT /home/$SSH_USERNAME/nginx/ $TARGET_SERVER_USERNAME@$TARGET_SERVER_IP:/home/$TARGET_SERVER_USERNAME/ && echo "Transfer successful!" || echo "Transfer failed"
+
+  echo "Transferring env files... [7/7]"
+  scp -i $TARGET_SERVER_KEYFILE_DIR -P $TARGET_SERVER_PORT /home/$SSH_USERNAME/.env $TARGET_SERVER_USERNAME@$TARGET_SERVER_IP:/home/$TARGET_SERVER_USERNAME/ && echo "Transfer successful!" || echo "Transfer failed"
+  scp -i $TARGET_SERVER_KEYFILE_DIR -P $TARGET_SERVER_PORT /home/$SSH_USERNAME/scripts/set_env.sh $TARGET_SERVER_USERNAME@$TARGET_SERVER_IP:/home/$TARGET_SERVER_USERNAME/ && echo "Transfer successful!" || echo "Transfer failed"
+fi
 
 echo "***** backup_server.sh Ended *****"
