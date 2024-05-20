c851e5a164e08a63b62a55dc254ec3d2fdfced54
GukJang <9ukjang@gmail.com>
2024-05-20 18:52:38 +0900

    이전 스프링 버전으로 rollback 스크립트 작성

---
+++ scripts/rollback_process.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+echo "***** Executing rollback_process.sh *****"
+
+# .env 파일 로드
+echo "Loading .env... [1/6]"
+ENV_FILE="$HOME/.env"
+if [ -f "$ENV_FILE" ]; then
+    export $(cat "$ENV_FILE" | xargs)
+    echo "$ENV_FILE exported"
+else
+    echo "Cannot find $ENV_FILE"
+    exit 1
+fi
+
+# OS 버전을 확인
+os_version=$(uname -a)
+
+if [[ $os_version == *"armv7l"* ]] || [[ $os_version == *"raspi"* ]]; then
+  echo "$os_version"
+
+  BLUE_CREATED=$(docker inspect --format '{{ .Created }}' "${DOCKER_USERNAME}/${SPRING_CONTAINER_NAME}:blue")
+  GREEN_CREATED=$(docker inspect --format '{{ .Created }}' "${DOCKER_USERNAME}/${SPRING_CONTAINER_NAME}:green")
+
+  if [[ "$BLUE_CREATED" > "$GREEN_CREATED" ]]; then
+    echo "Rollbacking to SPRING-GREEN"
+    docker compose -f docker-compose.pi.yml down spring-blue
+    docker compose -f docker-compose.pi.yml up -d spring-green
+  else
+    echo "Rollbacking to SPRING-BLUE"
+    docker compose -f docker-compose.pi.yml down spring-green
+    docker compose -f docker-compose.pi.yml up -d spring-blue
+  fi
+
+else
+  echo "$os_version"
+
+  BLUE_CREATED=$(docker inspect --format '{{ .Created }}' "${DOCKER_USERNAME}/${SPRING_CONTAINER_NAME}:blue")
+  GREEN_CREATED=$(docker inspect --format '{{ .Created }}' "${DOCKER_USERNAME}/${SPRING_CONTAINER_NAME}:green")
+
+  if [[ "$BLUE_CREATED" > "$GREEN_CREATED" ]]; then
+    echo "Rollbacking to SPRING-GREEN"
+    docker compose -f docker-compose.pi.yml down spring-blue
+    docker compose -f docker-compose.pi.yml up -d spring-green
+  else
+    echo "Rollbacking to SPRING-BLUE"
+    docker compose -f docker-compose.pi.yml down spring-green
+    docker compose -f docker-compose.pi.yml up -d spring-blue
+  fi
+fi
+
+echo "Sleeping for 30 seconds..."
+sleep 30
+
+echo "***** rollback_process.sh Ended *****"
