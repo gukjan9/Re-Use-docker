a9ce159c3295edea67baaa8c2a8f8a11c8887703
GukJang <9ukjang@gmail.com>
2024-05-20 19:00:34 +0900

    typo

--- scripts/rollback_process.sh
+++ scripts/rollback_process.sh
@@ -40,12 +40,12 @@ else
 
   if [[ "$BLUE_CREATED" > "$GREEN_CREATED" ]]; then
     echo "Rollbacking to SPRING-GREEN"
-    docker compose -f docker-compose.pi.yml down spring-blue
-    docker compose -f docker-compose.pi.yml up -d spring-green
+    docker compose -f docker-compose.yml down spring-blue
+    docker compose -f docker-compose.yml up -d spring-green
   else
     echo "Rollbacking to SPRING-BLUE"
-    docker compose -f docker-compose.pi.yml down spring-green
-    docker compose -f docker-compose.pi.yml up -d spring-blue
+    docker compose -f docker-compose.yml down spring-green
+    docker compose -f docker-compose.yml up -d spring-blue
   fi
 fi
 
