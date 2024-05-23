a654805eb7ab76080d2c026080710779ebdf3fd1
GukJang <9ukjang@gmail.com>
2024-05-23 22:48:54 +0900

    certbot 설치 후 갱신 명령어 crontab 에 추가 스크립트

---
+++ scripts/set_certbot.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo "***** Executing set_certbot.sh *****"
+
+# .env 파일 로드
+echo "Loading .env... [1/3]"
+ENV_FILE="$HOME/.env"
+if [ -f "$ENV_FILE" ]; then
+    export $(cat "$ENV_FILE" | xargs)
+else
+    echo "Cannot find $ENV_FILE"
+    exit 1
+fi
+
+# certbot 설치
+echo "Installing Certbot... [2/3]"
+sudo apt-get update -y
+sudo apt-get install software-properties-common -y
+sudo apt-get install certbot -y
+certbot --version
+
+# 스크립트 실행 성공 후 Crontab 설정
+echo "Setting Crontab... [3/3]"
+if [ $? -eq 0 ]; then
+  echo "Adding renewal job to crontab... [3/3]"
+  (crontab -l 2>/dev/null; echo "0 18 1 * * /usr/bin/certbot renew --renew-hook='sudo cp /etc/letsencrypt/live/reuse.kro.kr/fullchain.pem /home/${SSH_USERNAME}/nginx/conf.d/ && sudo cp /etc/letsencrypt/live/reuse.kro.kr/privkey.pem /home/${SSH_USERNAME}/nginx/conf.d/ && docker exec $NGINX_CONTAINER_NAME nginx -s reload'") | crontab -
+  echo "Crontab updated successfully"
+else
+  echo "Failed to setting crontab"
+fi
+
+echo "***** set_certbot.sh Ended *****"
