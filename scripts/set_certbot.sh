f7af3131d3b56475eabd266db4e34818670d6955
GukJang <9ukjang@gmail.com>
2024-05-24 00:36:08 +0900

    nginx 재시작 추가

--- scripts/set_certbot.sh
+++ scripts/set_certbot.sh
@@ -3,7 +3,7 @@
 echo "***** Executing set_certbot.sh *****"
 
 # .env 파일 로드
-echo "Loading .env... [1/3]"
+echo "Loading .env... [1/4]"
 ENV_FILE="$HOME/.env"
 if [ -f "$ENV_FILE" ]; then
     export $(cat "$ENV_FILE" | xargs)
@@ -13,16 +13,19 @@ else
 fi
 
 # certbot 설치
-echo "Installing Certbot... [2/3]"
+echo "Installing Certbot... [2/4]"
 sudo apt-get update -y
 sudo apt-get install software-properties-common -y
 sudo apt-get install certbot -y
 certbot --version
 
-# 스크립트 실행 성공 후 Crontab 설정
-echo "Setting Crontab... [3/3]"
+# nginx 재시작
+echo "Installing Certbot... [3/4]"
+docker exec $NGINX_CONTAINER_NAME nginx -s reload
+
+# 스크립트 실행 성공 후 crontab 설정
+echo "Setting Crontab... [4/4]"
 if [ $? -eq 0 ]; then
-  echo "Adding renewal job to crontab... [3/3]"
   (crontab -l 2>/dev/null; echo "0 18 1 * * /usr/bin/certbot renew --renew-hook='sudo cp /etc/letsencrypt/live/reuse.kro.kr/fullchain.pem /home/${SSH_USERNAME}/nginx/conf.d/ && sudo cp /etc/letsencrypt/live/reuse.kro.kr/privkey.pem /home/${SSH_USERNAME}/nginx/conf.d/ && docker exec $NGINX_CONTAINER_NAME nginx -s reload'") | crontab -
   echo "Crontab updated successfully"
 else
